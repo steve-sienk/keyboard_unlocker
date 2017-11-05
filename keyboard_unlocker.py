74e593a0ab9dde695c4707ccfad60adb81c4bf2b
Steve Sienkowski <stevesienkowski@StevesMac.local>
2017-11-04 23:38:52 -0400

    Add 30 second window of it being unlocked after password entered, add try/catch to allow for no keyboard error

--- keyboard_unlocker.py
+++ keyboard_unlocker.py
@@ -1,4 +1,5 @@
 import CHIP_IO.GPIO as GPIO
+import time
 
 from inputs import get_key
 from inputs import devices
@@ -12,30 +13,37 @@ GPIO.setup("CSID6", GPIO.OUT, initial=1)
 
 
 current_state=0
+unlock_time=0
 
 while True:
-     events = get_key()
-     for event in events:
-          print(event.ev_type, event.code, event.state)
-          if event.state==1 :
-               if current_state == 0 and event.code == 'KEY_1' :
-                    current_state = 1
-               elif current_state == 1 and event.code == 'KEY_2' :
-                    current_state = 2
-               elif current_state == 2 and event.code == 'KEY_3' :
-                    current_state = 3
-               else :
-                    current_state = 0
-                    
-     print('Current state: ', current_state)
-     
-     if current_state == 3 : 
-          GPIO.output("CSID0", GPIO.LOW)
-          GPIO.output("CSID2", GPIO.LOW)
-          GPIO.output("CSID4", GPIO.LOW)
-          GPIO.output("CSID6", GPIO.LOW)
-     else :
-          GPIO.output("CSID0", GPIO.HIGH)
-          GPIO.output("CSID2", GPIO.HIGH)
-          GPIO.output("CSID4", GPIO.HIGH)
-          GPIO.output("CSID6", GPIO.HIGH)
+     try:
+          events = get_key()
+          for event in events:
+               print(event.ev_type, event.code, event.state)
+               if event.state==1 :
+                    if current_state == 0 and event.code == 'KEY_1' :
+                         current_state = 1
+                    elif current_state == 1 and event.code == 'KEY_2' :
+                         current_state = 2
+                    elif current_state == 2 and event.code == 'KEY_3' :
+                         current_state = 3
+                         unlock_time = time.localtime
+                    else :
+                         current_state = 0
+                         
+          print('Current state: ', current_state)
+          
+          if current_state == 3 and (time.localtime-unlock_time < 20): 
+               GPIO.output("CSID0", GPIO.LOW)
+               GPIO.output("CSID2", GPIO.LOW)
+               GPIO.output("CSID4", GPIO.LOW)
+               GPIO.output("CSID6", GPIO.LOW)
+          else :
+               GPIO.output("CSID0", GPIO.HIGH)
+               GPIO.output("CSID2", GPIO.HIGH)
+               GPIO.output("CSID4", GPIO.HIGH)
+               GPIO.output("CSID6", GPIO.HIGH)
+          
+          
+     except:
+          print("Unexpected error:", sys.exc_info()[0])
