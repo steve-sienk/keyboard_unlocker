d4b9e065f38b72ee32d60034d43fbb442d4aeb7c
Steve Sienkowski <stevesienkowski@StevesMac.local>
2017-11-05 01:03:15 -0400

    iluvuyouluvme

--- keyboard_unlocker.py
+++ keyboard_unlocker.py
@@ -21,16 +21,19 @@ while True:
                GPIO.output("CSID4", GPIO.HIGH)
                GPIO.output("CSID6", GPIO.HIGH)
      
-          events = get_key()
-          for event in events:
-               print(event.ev_type, event.code, event.state)
-               if event.state==1 :
-                    if current_state == 0 and event.code == 'KEY_D' :
-                         current_state = 1
-                    elif current_state == 1 and event.code == 'KEY_A' :
-                         current_state = 2
-                    elif current_state == 2 and event.code == 'KEY_N' :
-                         current_state = 3
-                         unlock_time = time.time()
-                    else :
-                         current_state = 0
+          try:
+               events = get_key()
+               for event in events:
+                    print(event.ev_type, event.code, event.state)
+                    if event.state==1 :
+                         if current_state == 0 and event.code == 'KEY_D' :
+                              current_state = 1
+                         elif current_state == 1 and event.code == 'KEY_A' :
+                              current_state = 2
+                         elif current_state == 2 and event.code == 'KEY_N' :
+                              current_state = 3
+                              unlock_time = time.time()
+                         else :
+                              current_state = 0
+          except:
+               print "some error"
