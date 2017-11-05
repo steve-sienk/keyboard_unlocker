3445b3a0a2c86fcbd05238135711beb542a96beb
Steve Sienkowski <stevesienkowski@StevesMac.local>
2017-11-04 23:46:03 -0400

    change directory to root/kb_u/..py

--- run_unlocker.sh
+++ run_unlocker.sh
@@ -3,7 +3,7 @@ variable=$(ps -ef | grep keyboard_unlocker | wc -l | awk '{print $1}')
 echo $variable
 if [ $variable \< 2 ]
     then
-        python /root/keyboard_unlocker.py
+        python /root/keyboard_unlocker/keyboard_unlocker.py
 fi
 
 echo unlocker done
