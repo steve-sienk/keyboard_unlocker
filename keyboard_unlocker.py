5ff5aca663d66ee3fb4b8f91d46605e1747d1dd2
Steve Sienkowski <stevesienkowski@StevesMac.local>
2017-11-05 00:58:17 -0400

    ok

--- keyboard_unlocker.py
+++ keyboard_unlocker.py
@@ -6,16 +6,20 @@ from inputs import devices
 for device in devices:
      print(device)
 
-GPIO.setup("CSID0", GPIO.OUT, initial=1)
-GPIO.setup("CSID2", GPIO.OUT, initial=1)
 GPIO.setup("CSID4", GPIO.OUT, initial=1)
 GPIO.setup("CSID6", GPIO.OUT, initial=1)
-
 
 current_state=0
 unlock_time=0
 
-while True:
+while True:     
+          if ((time.time()-unlock_time) < 5.0): 
+               GPIO.output("CSID4", GPIO.LOW)
+               GPIO.output("CSID6", GPIO.LOW)
+          else :
+               GPIO.output("CSID4", GPIO.HIGH)
+               GPIO.output("CSID6", GPIO.HIGH)
+     
           events = get_key()
           for event in events:
                print(event.ev_type, event.code, event.state)
@@ -29,16 +33,3 @@ while True:
                          unlock_time = time.time()
                     else :
                          current_state = 0
-                         
-          print('Current state: ', current_state)
-          
-          if (current_state==3 or ((time.time()-unlock_time) < 5.0)): 
-               GPIO.output("CSID0", GPIO.LOW)
-               GPIO.output("CSID2", GPIO.LOW)
-               GPIO.output("CSID4", GPIO.LOW)
-               GPIO.output("CSID6", GPIO.LOW)
-          else :
-               GPIO.output("CSID0", GPIO.HIGH)
-               GPIO.output("CSID2", GPIO.HIGH)
-               GPIO.output("CSID4", GPIO.HIGH)
-               GPIO.output("CSID6", GPIO.HIGH)
