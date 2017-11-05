3d85c32450d74917da3dd36099205d8a7b041b67
Steve Sienkowski <stevesienkowski@StevesMac.local>
2017-11-05 12:16:22 -0500

    removed try/catch block. chron job runs every minute to restart if something failed.

--- keyboard_unlocker.py
+++ keyboard_unlocker.py
@@ -13,27 +13,25 @@ current_state=0
 unlock_time=0
 
 while True:
-          print("current state: ", current_state)
-          if ((time.time()-unlock_time) < 5.0): 
+          print("current state: ", current_state)          
+          if ((time.time()-unlock_time) < 5.0):
+               print("Unlocking for 5 seconds")
                GPIO.output("CSID4", GPIO.LOW)
                GPIO.output("CSID6", GPIO.LOW)
           else :
                GPIO.output("CSID4", GPIO.HIGH)
                GPIO.output("CSID6", GPIO.HIGH)
-     
-          try:
-               events = get_key()
-               for event in events:
-                    print(event.ev_type, event.code, event.state)
-                    if event.state==1 :
-                         if current_state == 0 and event.code == 'KEY_D' :
-                              current_state = 1
-                         elif current_state == 1 and event.code == 'KEY_A' :
-                              current_state = 2
-                         elif current_state == 2 and event.code == 'KEY_N' :
-                              current_state = 3
-                              unlock_time = time.time()
-                         else :
-                              current_state = 0
-          except:
-               print "some error"
+
+          events = get_key()
+          for event in events:
+               print(event.ev_type, event.code, event.state)
+               if event.state==1 :
+                    if current_state == 0 and event.code == 'KEY_D' :
+                         current_state = 1
+                    elif current_state == 1 and event.code == 'KEY_A' :
+                         current_state = 2
+                    elif current_state == 2 and event.code == 'KEY_N' :
+                         current_state = 3
+                         unlock_time = time.time()
+                    else :
+                         current_state = 0
